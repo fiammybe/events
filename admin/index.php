0400141cce6e0cbe7bf6fd511b33053c65d0c763
David <david.j@impresscms.org>
2023-09-01 20:53:57 +0200

    move code to the top folder

---
+++ admin/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Admin index page of the module
+ *
+ * Including the event page
+ *
+ * @copyright	Copyright Madfish (Simon Wilkinson) 2013.
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @since		1.0
+ * @author		Madfish (Simon Wilkinson) <simon@isengard.biz>
+ * @package		events
+ * @version		$Id$
+ */
+
+header("location: event.php");
+exit;
---
+++ language/english/admin.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * English language constants used in admin section of the module
+ *
+ * @copyright	Copyright Madfish (Simon Wilkinson) 2013.
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @since		1.0
+ * @author		Madfish (Simon Wilkinson) <simon@isengard.biz>
+ * @package		events
+ * @version		$Id$
+ */
+
+defined("ICMS_ROOT_PATH") or die("ICMS root path not defined");
+
+// Requirements
+define("_AM_EVENTS_REQUIREMENTS", "Events Requirements");
+define("_AM_EVENTS_REQUIREMENTS_INFO", "We've reviewed your system, unfortunately it doesn't meet all the requirements needed for Events to function. Below are the requirements needed.");
+define("_AM_EVENTS_REQUIREMENTS_ICMS_BUILD", "Events requires at least ImpressCMS 1.3.");
+define("_AM_EVENTS_REQUIREMENTS_SUPPORT", "Should you have any question or concerns, please visit our forums at <a href='http://community.impresscms.org'>http://community.impresscms.org</a>.");
+
+// Event
+define("_AM_EVENTS_EVENTS", "Events");
+define("_AM_EVENTS_EVENTS_DSC", "All events in the module");
+define("_AM_EVENTS_EVENT_CREATE", "Add a event");
+define("_AM_EVENTS_EVENT", "Event");
+define("_AM_EVENTS_EVENT_CREATE_INFO", "Fill-out the following form to create a new event.");
+define("_AM_EVENTS_EVENT_EDIT", "Edit this event");
+define("_AM_EVENTS_EVENT_EDIT_INFO", "Fill-out the following form in order to edit this event.");
+define("_AM_EVENTS_EVENT_MODIFIED", "The event was successfully modified.");
+define("_AM_EVENTS_EVENT_CREATED", "The event has been successfully created.");
+define("_AM_EVENTS_EVENT_VIEW", "Event info");
+define("_AM_EVENTS_EVENT_VIEW_DSC", "Here is the info about this event.");
+define("_AM_EVENTS_EVENT_ONLINE", "Event toggled online");
+define("_AM_EVENTS_EVENT_OFFLINE", "Event toggled offline");
+
+// New in V1.02
+define("_AM_EVENTS_FILTER_BY_TAG", "Filter by tag");
+define("_AM_EVENTS_ALL_EVENTS", "-- All --");
