0d802b62a60e7c60dcfc6fd241fdf844d716e333
David Janssens <fiammybe@gmail.com>
2023-09-13 10:00:32 +0200

    New translations common.php (Dutch)

--- src/language/Dutch/common.php
+++ src/language/Dutch/common.php
@@ -13,52 +13,52 @@
 defined("ICMS_ROOT_PATH") or die("ICMS root path not defined");
 
 // Event
-define("_CO_EVENTS_EVENT_TITLE", "Title");
-define("_CO_EVENTS_EVENT_TITLE_DSC", "Name of the event.");
-define("_CO_EVENTS_EVENT_DATE", "Start date");
-define("_CO_EVENTS_EVENT_DATE_DSC", "When does the event start?");
-define("_CO_EVENTS_EVENT_END_DATE", "End date");
-define("_CO_EVENTS_EVENT_END_DATE_DSC", "When does the event finish?");
-define("_CO_EVENTS_EVENT_DESCRIPTION", "Description");
-define("_CO_EVENTS_EVENT_DESCRIPTION_DSC", "Enter the details of your event (optional). If you do not provide a description, the module will link out to the event website instead (if available).");
-define("_CO_EVENTS_EVENT_COVERAGE", "Location");
-define("_CO_EVENTS_EVENT_COVERAGE_DSC", "Where is the event? Suggested format: City, Country or similar.");
+define("_CO_EVENTS_EVENT_TITLE", "Titel");
+define("_CO_EVENTS_EVENT_TITLE_DSC", "Naam van het evenement.");
+define("_CO_EVENTS_EVENT_DATE", "Begin datum");
+define("_CO_EVENTS_EVENT_DATE_DSC", "Wanneer begint het evenement?");
+define("_CO_EVENTS_EVENT_END_DATE", "Eind datum");
+define("_CO_EVENTS_EVENT_END_DATE_DSC", "Wanneer eindigt het evenement?");
+define("_CO_EVENTS_EVENT_DESCRIPTION", "Beschrijving");
+define("_CO_EVENTS_EVENT_DESCRIPTION_DSC", "Voer de details van uw evenement in (optioneel). Indien u geen beschrijving opgeeft, wordt de module gekoppeld aan de evenement website (indien beschikbaar).");
+define("_CO_EVENTS_EVENT_COVERAGE", "Locatie");
+define("_CO_EVENTS_EVENT_COVERAGE_DSC", "Waar is het evenement? Voorgesteld formaat: Stad, Land of iets vergelijkbaars.");
 define("_CO_EVENTS_EVENT_IDENTIFIER", "Website");
-define("_CO_EVENTS_EVENT_IDENTIFIER_DSC", "Enter a link to the official event website, if available.");
+define("_CO_EVENTS_EVENT_IDENTIFIER_DSC", "Voer een link in naar de officiële website van het evenement, indien beschikbaar.");
 define("_CO_EVENTS_EVENT_ONLINE_STATUS", "Online?");
-define("_CO_EVENTS_EVENT_ONLINE_STATUS_DSC", "Toggle the event on or offline. Events that are offline will not be visible and will not be returned in search results.");
-define("_CO_EVENTS_EVENT_CREATOR", "Submitter");
-define("_CO_EVENTS_EVENT_CREATOR_DSC", "Person that submitted this event.");
-define("_CO_EVENTS_EVENT_ONLINE", "Event is online");
-define("_CO_EVENTS_EVENT_OFFLINE", "Event is offline");
+define("_CO_EVENTS_EVENT_ONLINE_STATUS_DSC", "Schakelijk het evenement offline of online. Offline evenementen worden niet getoond in de zoekresultaten.");
+define("_CO_EVENTS_EVENT_CREATOR", "Inzender");
+define("_CO_EVENTS_EVENT_CREATOR_DSC", "Persoon die dit evenement heeft ingediend.");
+define("_CO_EVENTS_EVENT_ONLINE", "Evenement is online");
+define("_CO_EVENTS_EVENT_OFFLINE", "Evenement is offline");
 
 // Calendar
-define("_CO_EVENTS_CAL_JANUARY", "January");
-define("_CO_EVENTS_CAL_FEBRUARY", "February");
-define("_CO_EVENTS_CAL_MARCH", "March");
-define("_CO_EVENTS_CAL_APRIL", "April");
-define("_CO_EVENTS_CAL_MAY", "May");
-define("_CO_EVENTS_CAL_JUNE", "June");
-define("_CO_EVENTS_CAL_JULY", "July");
-define("_CO_EVENTS_CAL_AUGUST", "August");
-define("_CO_EVENTS_CAL_SEPTEMBER", "September");
-define("_CO_EVENTS_CAL_OCTOBER,", "October");
-define("_CO_EVENTS_CAL_NOVEMBER", "November");
-define("_CO_EVENTS_CAL_DECEMBER", "December");
+define("_CO_EVENTS_CAL_JANUARY", "januari");
+define("_CO_EVENTS_CAL_FEBRUARY", "februari");
+define("_CO_EVENTS_CAL_MARCH", "maart");
+define("_CO_EVENTS_CAL_APRIL", "april");
+define("_CO_EVENTS_CAL_MAY", "mei");
+define("_CO_EVENTS_CAL_JUNE", "juni");
+define("_CO_EVENTS_CAL_JULY", "juli");
+define("_CO_EVENTS_CAL_AUGUST", "augustus");
+define("_CO_EVENTS_CAL_SEPTEMBER", "september");
+define("_CO_EVENTS_CAL_OCTOBER,", "oktober");
+define("_CO_EVENTS_CAL_NOVEMBER", "november");
+define("_CO_EVENTS_CAL_DECEMBER", "december");
 
 // Index page
-define("_CO_EVENTS_NO_EVENTS", "There are no upcoming events listed at this time.");
+define("_CO_EVENTS_NO_EVENTS", "Er zijn op dit moment geen toekomste evenementen.");
 
 // Errors
-define("_CO_EVENTS_ERROR_BAD_DATE", "Start date MUST precede end date.");
-define("_CO_EVENTS_ERROR_BAD_URL", "Error: URL is not valid. Please check format and try again.");
+define("_CO_EVENTS_ERROR_BAD_DATE", "Begindatum MOET voor de einddatum liggen.");
+define("_CO_EVENTS_ERROR_BAD_URL", "Fout: URL is niet geldig. Controleer het formaat en probeer het opnieuw.");
 
 // New in V1.02
-define("_CO_EVENTS_EVENT_TAG", "Tags");
-define("_CO_EVENTS_EVENT_TAG_DSC", "Select the tags (subjects) you wish to label this event with.");
-define("_CO_EVENTS_ALL", "All");
-define("_CO_EVENTS_ALL_TAGS", "-- All --");
-define("_CO_EVENTS_UNTAGGED", "Untagged");
+define("_CO_EVENTS_EVENT_TAG", "tags");
+define("_CO_EVENTS_EVENT_TAG_DSC", "Selecteer de tags (onderwerpen) waarmee u dit evenement wilt labelen.");
+define("_CO_EVENTS_ALL", "Alles");
+define("_CO_EVENTS_ALL_TAGS", "-- Alles --");
+define("_CO_EVENTS_UNTAGGED", "Ongetagd");
 define("_CO_EVENTS_EVENT_IMAGE", "Logo");
-define("_CO_EVENTS_EVENT_IMAGE_DSC", "The logo or image of the event. Only useful for events where 
-	you are providing a full description, as otherwise it will not be displayed.");
+define("_CO_EVENTS_EVENT_IMAGE_DSC", "Het logo of afbeelding van de gebeurtenis. Alleen handig voor gebeurtenissen waarbij 
+	× je een volledige beschrijving geeft, anders wordt het niet getoond.");
