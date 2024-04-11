c4b4fbf8c336b888604a4f6b0fb7757ed48f5437
David Janssens <fiammybe@gmail.com>
2024-04-11 23:52:30 +0200

    NL vertalingen

---
+++ src/language/nederlands/admin.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * English language constants used in admin section of the module
+ *
+ * @copyright	Copyright Madfish (Simon Wilkinson) 2013.
+ * @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+ * @since		1.0
+ * @author		Madfish (Simon Wilkinson) <simon@isengard.biz>
+ * @package		events
+ * @version		$Id$
+ */
+
+defined("ICMS_ROOT_PATH") or die("ICMS root path not defined");
+
+// Requirements
+define("_AM_EVENTS_REQUIREMENTS", "Events module vereisten");
+define("_AM_EVENTS_REQUIREMENTS_INFO", "Een controle van uw systeem geeft aan dat niet aan alle vereisten is voldaan om de activiteiten module te laten werken.");
+define("_AM_EVENTS_REQUIREMENTS_ICMS_BUILD", "Events vereist ten minste ImpressCMS 1.4.5.");
+define("_AM_EVENTS_REQUIREMENTS_SUPPORT", "Voor assistentie, bezoek ons forum op <a href='https://www.impresscms.org/modules/iforum/viewforum.php?forum=9'>https://www.impresscms.org</a>.");
+
+// Event
+define("_AM_EVENTS_EVENTS", "Activiteiten");
+define("_AM_EVENTS_EVENTS_DSC", "Alle activiteiten");
+define("_AM_EVENTS_EVENT_CREATE", "Een activiteit toevoegen");
+define("_AM_EVENTS_EVENT_CLONE", "Kloon een activiteit");
+define("_AM_EVENTS_EVENT", "Activiteit");
+define("_AM_EVENTS_EVENT_CREATE_INFO", "Vul het onderstaande formulier in om een activiteit aan te maken.");
+define("_AM_EVENTS_EVENT_EDIT", "Bewerk deze activiteit");
+define("_AM_EVENTS_EVENT_EDIT_INFO", "Vul het onderstaande formulier in om een activiteit te bewerken.");
+define("_AM_EVENTS_EVENT_MODIFIED", "De activiteit werd met succes bewerkt.");
+define("_AM_EVENTS_EVENT_CREATED", "De activiteit werd met succes aangemaakt.");
+define("_AM_EVENTS_EVENT_VIEW", "Activiteit informatie");
+define("_AM_EVENTS_EVENT_VIEW_DSC", "Hieronder informatie over de activiteit.");
+define("_AM_EVENTS_EVENT_ONLINE", "De activiteit werd online geplaatst");
+define("_AM_EVENTS_EVENT_OFFLINE", "De activiteit werd offline geplaatst");
+
+// New in V1.02
+define("_AM_EVENTS_FILTER_BY_TAG", "Filteren op categorie");
+define("_AM_EVENTS_ALL_EVENTS", "-- Alles --");
---
+++ src/language/nederlands/blocks.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* English language constants used in blocks of the module
+*
+* @copyright	Copyright Madfish (Simon Wilkinson) 2013.
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @since		1.0
+* @author		Madfish (Simon Wilkinson) <simon@isengard.biz>
+* @package		events
+* @version		$Id$
+*/
+
+defined("ICMS_ROOT_PATH") or die("ICMS root path not defined");
+
+// Upcoming events block
+define("_MB_EVENTS_EVENT_UPCOMING_LIMIT", "Aantal aankomende evenementen om weer te geven: ");
+define("_MB_EVENTS_EVENT_TITLE_LENGTH", "Maximale titel lengte: ");
+
+// New in V1.02
+define("_MB_EVENTS_ALL_TAGS", "-- Alles --");
+define("_MB_EVENTS_FILTER_BY_TAG", "Filteren op tag");
+define("_MB_EVENTS_DYNAMIC_TAG", "Dynamische filteren op tag? (Overschrijft statische tag filter).");
+define("_MB_EVENTS_EVENT_YES", "Ja");
+define("_MB_EVENTS_EVENT_NO", "Neen");
